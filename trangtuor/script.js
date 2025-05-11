9de7a9d35f3d217f6f240aac6a3d857989c1e949
HBinSD <135139301+HBinSD@users.noreply.github.com>
2025-05-11 22:09:10 +0700

    Add files via upload

--- trangtuor/script.js
+++ trangtuor/script.js
@@ -1,17 +1,11 @@
-function goToPost(tourId) {
-    // Giả lập chuyển trang sang bài viết tương ứng
-    // Trong thực tế bạn có thể thay bằng đường link thật
-    const tourLinks = {
-      "hanoi-1ngay": "baiviet/hanoi-1ngay.html",
-      "hanoi-3ngay": "baiviet/hanoi-3ngay.html",
-      "hanoi-hatay": "baiviet/hanoi-hatay.html",
-      "pho-co": "baiviet/pho-co.html"
-    };
-  
-    if (tourLinks[tourId]) {
-      window.location.href = tourLinks[tourId];
-    } else {
-      alert("Bài viết chưa có!");
-    }
-  }
-  
+document.querySelector('.search-button button').addEventListener('click', function () {
+    const keyword = document.getElementById('input').value.toLowerCase();
+    const cards = document.querySelectorAll('.tour-card');
+
+    cards.forEach(cards => {
+      const title = cards.querySelector('.tour-text').textContent.toLowerCase();
+
+      const matchKeyword = keyword === "" || title.includes(keyword);
+      cards.style.display = (matchKeyword) ? 'block' : 'none';
+    });
+  });
